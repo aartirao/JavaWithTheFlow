50e6649818b34c3a9c1c711194921af47607a10e
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2016-04-30 19:43:50 -0700

    bubble cloud work

--- tagger.py
+++ tagger.py
@@ -16,17 +16,18 @@ connection = pymysql.connect(host=config.get('database','host'),
 
 
 def getTags():
-    tags = set()
+    tags = {}
     i = 1
     with connection.cursor() as cursor:
-        query = "SELECT DISTINCT(Tags) FROM Posts"
+        query = "SELECT DISTINCT(P.Tags) as Tags, PT.TopicId as TopicId FROM Posts P join PostTopicMap PT on PT.PostId = P.Id"
         cursor.execute(query)
         posts = cursor.fetchall()
         for row in posts:            
             if row[u'Tags'] != None and row[u'Tags'] != "":
                 s = row[u'Tags'][1:-1]
                 a = s.split("><")
-                tags = tags | set(a)                
+                for each in a:
+                    tags[each] = row[u'TopicId']                            
             print i
             i = i + 1
     return tags
@@ -35,8 +36,8 @@ def putTags(tags):
     i = 1
     with connection.cursor() as cursor:
         for tag in list(tags):            
-            query = "INSERT INTO Tags(TagName) values (%s)"
-            cursor.execute(query, (tag))
+            query = "INSERT INTO Tags(TagName, TopicId) values (%s, %s)"
+            cursor.execute(query, (tag, tags[tag]))
             connection.commit()
             print "ins" + str(i) 
             i = i + 1
@@ -46,7 +47,14 @@ def getTagFrequency(userId):
     i = 1
     result = {}
     with connection.cursor() as cursor:
-       
+        query1 = "select TagName, TopicId from Tags"
+        cursor.execute(query1)
+        Tags = cursor.fetchall()
+        
+        tags = {}
+        for tag in Tags:
+            tags[tag[u'TagName']] = tag[u'TopicId']
+        
         query2 = "select TopicId, Weight from UserInterests where UserId = %s order by Weight desc Limit 5"
         cursor.execute(query2, (userId))
         Interests = cursor.fetchall()
@@ -72,16 +80,31 @@ def getTagFrequency(userId):
         res = {}
         res["name"] = "words"
         children = []
-        for (each, val) in resultlist:
+        childs = {}
+        
+        for (each, val) in resultlist[:150]:
             if each != "java":
-                children.append({"name":each, "size": val})
-        res["children"] = children[:150]
+                #children.append({"name":each, "size": val})
+                if tags[each] not in childs.keys():
+                    childs[tags[each]] = [{"name":each, "size": val}]
+                else:
+                    childs[tags[each]] = childs[tags[each]] + [{"name":each, "size": val}]
+        i = 0
+        
+        for each in childs.keys():                
+            i += 1
+            subchildren = []
+            #for e in childs[each]:
+            #    subchildren.append(e)
+            children.append({"name": i, "children":childs[each]})
+        print childs                
+        res["children"] = children
         return res
              
             
 
 if __name__ == "__main__":
-    # tags = getTags()   
-    # putTags(tags)
-    getTagFrequency("821742")        
+    #tags = getTags()   
+    #putTags(tags)
+    print getTagFrequency("821742")        
         
